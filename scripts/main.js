81ea703a4090feeea7651193e17e63c4a7759d77
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-06-01 19:50:15 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-
+let hmm = false;
+Events.on(ClientLoadEvent, () => {
+  if(!hmm){
+    Vars.ui.showOkText("[accent]Lazy Alert[]", "[purple]ShadowMod[] \n \nThe creator of this mod is too lazy to finish it. Please go to his repository and spam PRs asking [red]WHEN[] [purple]SHADOW MOD[] YOU LAZY BOI. /n /n    - By Flin",
+    () => {
+      hmm = true;
+    });
+  }
+});
