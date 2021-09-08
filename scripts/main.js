be410e78be05c03130c9295c19965c22c2189c4a
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-09-08 18:12:24 -0300

    soon™

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 let hmm = false;
 Events.on(ClientLoadEvent, () => {
   if(!hmm){
-    Vars.ui.showOkText("[accent]Lazy Alert[]", "[purple]ShadowMod[] \n \nThe creator of this mod is too lazy to finish it. Please go to his repository and spam PRs asking [red]WHEN[] [purple]SHADOW MOD[] YOU LAZY BOI. /n /n    - By Flin",
+    Vars.ui.showOkText("[accent]Lazy Alert[]", "[purple]ShadowMod[] \n \nThe creator of this mod is too lazy to finish it. Please go to his repository and spam PRs asking [red]WHEN[] [purple]SHADOW MOD[] YOU LAZY BOI. /n /n    - By Flin/n/nsoon™/n- pietro",
     () => {
       hmm = true;
     });
